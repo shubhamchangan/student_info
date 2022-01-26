4c5a0af37a565df29b5aed816e7ac51b9a3e4d53
shubham changan <98081243+shubhamchangan@users.noreply.github.com>
2022-01-26 20:11:42 +0530

    Add files via upload

---
+++ find_student_marks.py
@@ -0,0 +1,30 @@
+
+#WAP to accept student name, marks from user.
+#Also display the student name and marks
+
+n = int(input('Enter the number of students:'))
+for i in range(n):
+    name = input('Enter the name:')
+    marks = input('Enter your marks:')
+    print('Your name is:',name, 'marks obtained:', marks)
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
